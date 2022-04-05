ec403855e6a70d00166bb2c503537a2f51e01637
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-04-05 15:53:02 +0200

    fmt: update example fmt in according with the package rule
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- example/main.v
+++ example/main.v
@@ -1,11 +1,15 @@
 module main
 
 import datatypes
-import vbenchmark
+import vbenchamark
 
 fn assign_variable() {
 	mut x := 1
 	x = 2
+	// TODO this is only an example, during
+	// a benchmark you should never write to console
+	// it is a waste of time.
+	println(x)
 }
 
 fn inser_in_btree() {
@@ -16,9 +20,9 @@ fn inser_in_btree() {
 }
 
 fn main() {
-	vbenchmark.add_benchmark('Insert in BSTree', inser_in_btree)
-	vbenchmark.add_benchmark('Assign variable', assign_variable)
-	if !vbenchmark.run_benchamars() {
+	vbenchamark.add_benchmark('Insert in BSTree', inser_in_btree)
+	vbenchamark.add_benchmark('Assign variable', assign_variable)
+	if !run_benchamars() {
 		return
 	}
 }
